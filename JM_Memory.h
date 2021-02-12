bc11aaec70e25295b22999481ff82efebc7a2c53
Jo-Milk <74319993+Jo-Milk@users.noreply.github.com>
2021-02-12 11:56:54 -0500

    Create JM_Memory.h

---
+++ JM_Memory.h
@@ -0,0 +1,4 @@
+// I never released the content of this file 
+// basically you need to download what the server writes to the sprx when you go a valid key
+//Reflex V2.5 would have TOC with null offset following we had to put the right offsets next to the TOC of the game
+//so it would call the functions and not crash
